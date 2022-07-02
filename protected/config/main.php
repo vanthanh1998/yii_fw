f0db6072fac9a55c2347113c68e58b2009893c8e
Văn Thanh <vanthanh@jv-it.com.vn>
2022-07-03 01:03:04 +0700

    remove index.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -37,6 +37,7 @@ return array(
 
 		// uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
+            'showScriptName' => false,
 			'urlFormat'=>'path',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
